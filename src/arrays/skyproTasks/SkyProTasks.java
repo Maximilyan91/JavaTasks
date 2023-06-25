273221ad7327a51a492a71baff5d4e0406981a05
Максим <gmax7911@gmail.com>
2023-06-25 10:42:26 +0300

    массивы

---
+++ src/arrays/skyproTasks/SkyProTasks.java
@@ -0,0 +1,36 @@
+package arrays.skyproTasks;
+
+import java.util.Arrays;
+
+public class SkyProTasks {
+
+    public static void main(String[] args) {
+        /*
+         * Распечатайте на отдельной строчке элементы
+         * каждого массива в обратном порядке через запятую.
+         * В конце строки запятую ставить не надо
+         * */
+        System.out.println("Задача №1");
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] > 1) {
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.print(arr[i]);
+            }
+        }
+
+        /*Пройдитесь по целочисленному массиву
+         и все нечетные числа в нем сделайте четными*/
+
+        System.out.println("\nЗадача №2");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[i] += 1;
+            }System.out.print(arr[i] + " ");
+        }
+    }
+}
