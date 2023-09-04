3a7296eba92769d526c60d5fb10ea3f87a4a020c
Максим <gmax7911@gmail.com>
2023-09-04 09:36:30 +0300

    Напишите Java-программу для печати площади и периметра круга.

---
+++ src/kodesourceTasks/mainPart1/Task11.java
@@ -0,0 +1,24 @@
+package kodesourceTasks.mainPart1;
+
+import java.util.Scanner;
+
+public class Task11 {
+
+    /**
+     * Напишите Java-программу для печати площади и периметра круга.
+     */
+
+    public static void main(String[] args) {
+        double pi = Math.PI;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите радиус круга: ");
+        double range = in.nextInt();
+
+        double circleLength = 2 * pi * range;
+        System.out.println("Длина окружности равна: " + circleLength);
+
+
+        double areaCircle = pi * (range * range);
+        System.out.println("Площадь круга: " + areaCircle);
+    }
+}
