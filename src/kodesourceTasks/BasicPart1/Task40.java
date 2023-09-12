fdfb66942115087e7ab407d5277ee3a3d39b87ff
Максим <gmax7911@gmail.com>
2023-09-12 18:31:32 +0300

    Напишите программу на Java, чтобы перечислить доступные наборы символов в объектах charset

---
+++ src/kodesourceTasks/BasicPart1/Task40.java
@@ -0,0 +1,16 @@
+package kodesourceTasks.BasicPart1;
+
+import java.nio.charset.Charset;
+
+public class Task40 {
+
+    /**
+     * Напишите программу на Java, чтобы перечислить доступные наборы символов в объектах charset
+     */
+
+
+    public static void main(String[] args) {
+
+        System.out.println(Charset.availableCharsets());
+    }
+}
