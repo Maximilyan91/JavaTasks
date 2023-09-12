182672e6abe345e1cf78ab34488c94366e352995
Максим <gmax7911@gmail.com>
2023-09-12 19:14:33 +0300

    Напишите программу на Java для печати значения ascii данного символа.

---
+++ src/kodesourceTasks/BasicPart1/Task41.java
@@ -0,0 +1,23 @@
+package kodesourceTasks.BasicPart1;
+
+import java.util.Scanner;
+
+/**
+*  Напишите программу на Java для печати значения ascii данного символа. */
+public class Task41 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку для перевода в кодировку ASCII: ");
+        String line = in.nextLine();
+
+         char[] symbols = line.toCharArray();
+
+        for (char symbol : symbols) {
+            System.out.print("Значение в кодировке ASCII: " + (int) symbol);
+        }
+
+    }
+
+}
