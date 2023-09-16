bac942f89336166bc6271471f31b1be3ee250fc1
Максим <gmax7911@gmail.com>
2023-09-16 09:27:55 +0300

    Напишите программу на Java для отображения системного времени

---
+++ src/kodesourceTasks/BasicPart1/Task46.java
@@ -0,0 +1,11 @@
+package kodesourceTasks.BasicPart1;
+
+import java.time.LocalTime;
+
+/** Напишите программу на Java для отображения системного времени */
+public class Task46 {
+
+    public static void main(String[] args) {
+        System.out.println(LocalTime.now());
+    }
+}
